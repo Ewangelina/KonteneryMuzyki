4d7264ba64374705f154e213ab0ca9552d23347e
Ewangelina <235842@edu.p.lodz.pl>
2023-04-28 09:01:09 +0200

    Create wiki.py

---
+++ wiki.py
@@ -0,0 +1,22 @@
+url = 'https://en.wikipedia.org/wiki/Category:Songs_by_year'
+
+import requests
+import wikipediaapi
+#pip install wikipedia-api
+
+#https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Songs_by_year&cmtype=subcat
+
+
+wiki = wikipediaapi.Wikipedia('en')
+cat = wiki.page('Category:Songs_by_year')
+for p in cat.categorymembers.values():
+  if p.namespace == wikipediaapi.Namespace.CATEGORY:
+    name = str(p)
+    name = name.split(" (id:")[0]
+    print(name)
+    print(len(p.categorymembers))
+    print('')
+  elif p.namespace == wikipediaapi.Namespace.MAIN:
+    print(p)
+
+    
